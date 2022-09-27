22645991342dfc2323a1e4a1f1506b89b271b9f4
Frank <frank@parity.io>
2022-09-27 16:36:39 +0100

    feat: connect to custom chain spec

--- index.ts
+++ index.ts
@@ -1,20 +1,18 @@
-import { ScProvider, WellKnownChain } from "@polkadot/rpc-provider/substrate-connect";
-import { ApiPromise } from "@polkadot/api";
+import { ScProvider, WellKnownChain } from "@polkadot/rpc-provider/substrate-connect";import { ApiPromise } from "@polkadot/api";
+import jsonParachainSpec from "./statemint.json";
 
 window.onload = () => {
     void (async () => {
         try {
-            const provider = new ScProvider(WellKnownChain.polkadot);
+            const relayProvider = new ScProvider(WellKnownChain.polkadot);
+            const parachainSpec = JSON.stringify(jsonParachainSpec);
+            const provider = new ScProvider(parachainSpec, relayProvider);
 
             await provider.connect();
             const api = await ApiPromise.create({ provider });
-            await api.rpc.chain.subscribeNewHeads(
-                (lastHeader: { number: unknown; hash: unknown }) => {
-                    console.log(
-                        `New block #${lastHeader.number} has hash ${lastHeader.hash}`
-                    );
-                }
-            );
+            await api.rpc.chain.subscribeNewHeads((lastHeader: { number: unknown; hash: unknown }) => {
+                console.log(`New block #${lastHeader.number} has hash ${lastHeader.hash}`);
+            });
         } catch (error) {
             console.error(<Error>error);
         }
